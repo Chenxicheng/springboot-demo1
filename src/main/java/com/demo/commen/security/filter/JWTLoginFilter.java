af4d18a9dca516d778027e96934c64aa0f7cece0
Dashwood <cxc_dashwood@163.com>
2018-06-11 22:38:48 +0800

    jwt功能

---
+++ src/main/java/com/demo/commen/security/filter/JWTLoginFilter.java
@@ -0,0 +1,69 @@
+package com.demo.commen.security.filter;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.demo.commen.security.jwt.TokenAuthenticationService;
+import com.demo.commen.utils.JSONResult;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.io.IOUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
+
+
+public class JWTLoginFilter extends AbstractAuthenticationProcessingFilter {
+
+    @Autowired
+    private TokenAuthenticationService tokenAuthenticationService;
+
+    public JWTLoginFilter(String url, AuthenticationManager authManager) {
+        super(new AntPathRequestMatcher(url));
+        setAuthenticationManager(authManager);
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        // JSON反序列化成 AccountCredentials
+//        AccountCredentials creds = JSON.new ObjectMapper().readValue(req.getInputStream(), AccountCredentials.class);
+
+        String v = IOUtils.toString(req.getInputStream(), "UTF-8");
+        JSONObject json = JSON.parseObject(IOUtils.toString(req.getInputStream(), "UTF-8"));
+
+        System.out.println(json.toString());
+        // 返回一个验证令牌
+        return getAuthenticationManager().authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        json.getString("loginName"),
+                        json.getString("password")
+                )
+        );
+    }
+
+    @Override
+    protected void successfulAuthentication(
+            HttpServletRequest req,
+            HttpServletResponse res, FilterChain chain,
+            Authentication auth) throws IOException, ServletException {
+        tokenAuthenticationService.addAuthentication(res, auth.getName());
+    }
+
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getOutputStream().println(JSONResult.fillResultString(500, "Internal Server Error!!!", null));
+    }
+}
---
+++ src/main/java/com/demo/commen/security/jwt/TokenAuthenticationService.java
@@ -0,0 +1,99 @@
+package com.demo.commen.security.jwt;
+
+
+import com.demo.commen.utils.JSONResult;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class TokenAuthenticationService implements Serializable{
+
+    @Value("${jwt.expirationtime}")
+    private Long EXPIRATIONTIME;
+
+    @Value("${jwt.secret}")
+    private String SECRET;
+
+    public static final String TOKEN_PREFIX = "Bearer";        // Token前缀
+    public static final String HEADER_STRING = "Authorization";// 存放Token的Header Key
+
+
+    // JWT生成方法
+
+    /**
+     * JWT生成方法
+     * @param response
+     * @param username
+     */
+    public void addAuthentication(HttpServletResponse response, String username) {
+
+        // 生成JWT
+        String JWT = Jwts.builder()
+                // 保存权限（角色）
+                .claim("authorities", "ROLE_ADMIN,AUTH_WRITE")
+                // 用户名写入标题
+                .setSubject(username)
+                // 有效期设置
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                // 签名设置
+                .signWith(SignatureAlgorithm.HS512, SECRET)
+                .compact();
+
+        // 将 JWT 写入 body
+        try {
+            response.setContentType("application/json");
+            response.setStatus(HttpServletResponse.SC_OK);
+            response.getOutputStream().println(JSONResult.fillResultString(0, "", JWT));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 负责认证JWT最后生成验证令牌
+     * @param request
+     * @return
+     */
+    public Authentication getAuthentication(HttpServletRequest request) {
+        // 从Header中拿到token
+        String token = request.getHeader(HEADER_STRING);
+
+        if (token != null) {
+            // 解析 Token
+            Claims claims = Jwts.parser()
+                    // 验签
+                    .setSigningKey(SECRET)
+                    // 去掉 Bearer
+                    .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
+                    .getBody();
+
+            // 拿用户名
+            String user = claims.getSubject();
+
+            // 得到 权限（角色）
+            List<GrantedAuthority> authorities =  AuthorityUtils.commaSeparatedStringToAuthorityList((String) claims.get("authorities"));
+
+            // 返回验证令牌
+            return user != null ?
+                    new UsernamePasswordAuthenticationToken(user, null, authorities) :
+                    null;
+        }
+        return null;
+    }
+
+
+}
